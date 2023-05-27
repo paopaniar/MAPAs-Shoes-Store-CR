d40bc02655e259b20f1501e7f391d7f594ddeae8
MARIANELA ROMERO MENDEZ <mromerom@est.utn.ac.cr>
2023-05-27 13:39:24 -0600

    rol

---
+++ server/prisma/seed/rol.ts
@@ -0,0 +1,14 @@
+export const rol = [
+    //1
+    {
+      nombre: "Administrador",
+    },
+    //2
+    {
+      nombre: "Vendedor",
+    },
+    //3
+    {
+      nombre: "Cliente",
+    },
+  ];
