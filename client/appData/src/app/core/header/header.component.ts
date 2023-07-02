525601d9db50170a13bad5d22c16d1ca49898e8d
paopaniar <panipao0@gmail.com>
2023-07-01 18:21:01 -0600

    header

--- client/appData/src/app/core/header/header.component.ts
+++ client/appData/src/app/core/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  isAutenticated: boolean;
+  currentUser: any;
 
+  constructor() {
+    //afecta donde de se pone la lógica (construtor o OnInit) solo si se tiene que cargar una parte de la pagina
+  }
+
+  ngOnInit(): void {
+    //valores de prueba
+    this.isAutenticated = false;
+    let user={
+      nombre:"Administrador Mapas",
+      email:"admin@gmail.com",
+
+    }
+    this.currentUser=user; 
+  }
 }
