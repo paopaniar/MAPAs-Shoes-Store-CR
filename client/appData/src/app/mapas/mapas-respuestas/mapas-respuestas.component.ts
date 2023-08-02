684f1e58e98f2d9d5ac1c2feacb134136c1ddbfe
paopaniar <panipao0@gmail.com>
2023-08-02 11:40:59 -0600

    mapas respuestas

--- client/appData/src/app/mapas/mapas-respuestas/mapas-respuestas.component.ts
+++ client/appData/src/app/mapas/mapas-respuestas/mapas-respuestas.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-mapas-respuestas',
   templateUrl: './mapas-respuestas.component.html',
@@ -8,3 +9,13 @@ import { Component } from '@angular/core';
 export class MapasRespuestasComponent {
   datos:any;
 }
+
+//  ListaPreguntas(id:Number){
+//   console.log(id);
+//   const dialogConfig = new MatDialogConfig();
+//   dialogConfig.disableClose = false;
+//   dialogConfig.data = {
+//     id: id,
+//   };
+//   this.dialog.open(MapasDiagComponent, dialogConfig);
+// }
