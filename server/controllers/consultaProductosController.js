b5f05ee5b255e46716ef18a6746fd55adab31640
paopaniar <panipao0@gmail.com>
2023-08-02 15:45:16 -0600

    consulta

--- server/controllers/consultaProductosController.js
+++ server/controllers/consultaProductosController.js
@@ -25,37 +25,43 @@ module.exports.getByProducto = async (request, response, next) => {
 };
 
 
-module.exports.createResponses = async (request, response, next) => {
+module.exports.createResponseForMessage = async (request, response, next) => {
     try {
-        const { respuesta } = request.body;
-        const idConsultaProducto = parseInt(request.params.id);
-
-        // Fetch the consultaProductos based on the given ID
-        const consultaProducto = await prisma.consultaProductos.findUnique({
-            where: {
-                id: idConsultaProducto,
-            },
-        });
-
-        if (!consultaProducto) {
-            return response.status(404).json({ error: 'ConsultaProducto not found' });
-        }
-
-        // Insert the new response into the database
-        const newRespuesta = await prisma.respuesta.create({
-            data: {
-                respuesta: respuesta,
-                productoId: consultaProducto.productoId,
-                usuarioId: 1, // Replace 1 with the actual user ID from the request or your authentication mechanism
-            },
-        });
-
-        response.json(newRespuesta);
+      const { respuesta } = request.body;
+      const idConsultaProducto = parseInt(request.params.id);
+  
+      // Fetch the consultaProductos based on the given ID
+      const consultaProducto = await prisma.consultaProductos.findUnique({
+        where: {
+          id: idConsultaProducto,
+        },
+      });
+  
+      if (!consultaProducto) {
+        return response.status(404).json({ error: 'ConsultaProducto not found' });
+      }
+  
+      // Update the existing consultaProducto with the new response
+      const updatedConsultaProducto = await prisma.consultaProductos.update({
+        where: {
+          id: consultaProducto.id,
+        },
+        data: {
+          respuesta: respuesta,
+        },
+      });
+  
+      response.json(updatedConsultaProducto);
     } catch (error) {
-        console.error('Error creating response:', error);
-        response.status(500).json({ error: 'Internal server error' });
+      console.error('Error creating response:', error);
+      response.status(500).json({ error: 'Internal server error' });
     } finally {
-        await prisma.$disconnect(); // Make sure to disconnect from Prisma when done
+      await prisma.$disconnect(); // Make sure to disconnect from Prisma when done
     }
-};
+  };
+  
+  
+
+
+
 
--- server/routes/consultaProductosRoutes.js
+++ server/routes/consultaProductosRoutes.js
@@ -6,11 +6,6 @@ const auth=require("../middleware/auth");
 
 router.get("/", consultaProductoController.get);
 router.get('/:id',consultaProductoController.getByProducto);
-// router.post('/respuesta/:id', consultaProductoController.createResponses);
-router.post('/:productoId/consultas/:id', consultaProductoController.createResponses);
-
-// Suponiendo que tienes rutas RESTful para productos y consultasProductos
-// router.post('/productos/:productoId/consultas/:idConsultaProducto/respuesta', consultaProductoController.createResponses);
-
+router.put('/consultas/:id/respuesta', consultaProductoController.createResponseForMessage);
 
 module.exports = router;
