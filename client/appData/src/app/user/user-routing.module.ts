94fffc1592f564997bf497ed9ffd9bdd289dd049
paopaniar <panipao0@gmail.com>
2023-08-17 13:55:50 -0600

    user list

--- client/appData/src/app/user/user-routing.module.ts
+++ client/appData/src/app/user/user-routing.module.ts
@@ -4,6 +4,7 @@ import { UserIndexComponent } from './user-index/user-index.component';
 import { UserCreateComponent } from './user-create/user-create.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { AuthGuard } from '../share/guards/auth.guard';
 import { UserListInactivosComponent } from './user-list-inactivos/user-list-inactivos.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
 
@@ -18,8 +19,16 @@ const routes: Routes = [
   },
   {path: 'usuario/perfil/:id', component: UserEditComponent},
 
-  {path: 'usuario/lista', component: UserListComponent},
-  {path: 'usuario/inactivo', component: UserListInactivosComponent}
+  {path: 'usuario/lista', component: UserListComponent,
+  canActivate:[AuthGuard],
+  data:{
+    roles: ['ADMIN']
+  }},
+  {path: 'usuario/inactivo', component: UserListInactivosComponent,
+canActivate:[AuthGuard],
+  data:{
+    roles: ['ADMIN']
+  }}
 
 ];
 
